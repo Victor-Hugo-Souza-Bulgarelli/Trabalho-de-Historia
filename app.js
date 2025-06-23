35a721e6952149637c09af12d7b8d1c719852153
Victor <victor.hugodesouza.bulgarelli@gmail.com>
2025-06-23 16:35:17 -0300

    update

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ document.addEventListener("DOMContentLoaded", function() {
   const navbar = document.querySelector('.navbar');
   const logo = document.getElementById('navbar-logo');
   const favicon = document.querySelector('link[rel="shortcut icon"]');
+  const footer = document.querySelector('.footer');
+
   btn.addEventListener('click', function() {
     if(navbar.classList.contains("tema-israel")){
       navbar.classList.remove('tema-israel');
       navbar.classList.add('tema-palestina');
-      logo.src = "https://upload.wikimedia.org/wikipedia/commons/0/00/Flag_of_Palestine.svg"; // imagem para tema palestina
+      logo.src = "https://upload.wikimedia.org/wikipedia/commons/0/00/Flag_of_Palestine.svg";
       favicon.href = "https://upload.wikimedia.org/wikipedia/commons/0/00/Flag_of_Palestine.svg";
+      footer.classList.remove('tema-israel-footer');
+      footer.classList.add('tema-palestina-footer');
     } else {
       navbar.classList.remove('tema-palestina');
       navbar.classList.add('tema-israel');
-      logo.src = "https://img.icons8.com/?size=100&id=1986&format=png&color=FFFFFF"; // imagem para tema israel
+      logo.src = "https://img.icons8.com/?size=100&id=1986&format=png&color=FFFFFF";
       favicon.href = "https://img.icons8.com/?size=100&id=1986&format=png&color=FFFFFF";
+      footer.classList.remove('tema-palestina-footer');
+      footer.classList.add('tema-israel-footer');
     }
   });
 });
